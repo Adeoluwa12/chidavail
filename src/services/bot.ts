109463967498f4d24ee618d9bb982000c8cb697a
Adeoluwa12 <adeoyeoluwaferanmi@gmail.com>
2025-04-29 02:59:47 +0100

    all yes and amen

--- src/services/bot.ts
+++ src/services/bot.ts
@@ -348,7 +348,7 @@ export async function setupBot(): Promise<void> {
     await closeBrowser()
 
     browser = await puppeteer.launch({
-      headless: true,
+      headless: false,
       args: [
         "--no-sandbox",
         "--disable-setuid-sandbox",
